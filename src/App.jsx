7910f95b78f702b7ba508406a2970e3ad3150583
eberzina <berzina3633@gmail.com>
2026-03-06 15:10:39 +0200

    Install react router and create two pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import {Routes, Route} from "react-router-dom";
+import {Subscribe} from "./pages/Subscribe";
+import {ThankYou} from "./pages/ThankYou";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <Routes>
+      <Route index element={<Subscribe />} />
+      <Route path="/ThankYou" element={<ThankYou />} />
+    </Routes>
+  );
 }
 
 export default App
